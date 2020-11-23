8defb1878905aa5389e879b60b606f7c3695171f
jasonam234@gmail.com <jasonam234@gmail.com>
2020-11-23 21:23:23 +0700

    testing without auth

--- smpn/smpn/settings.py
+++ smpn/smpn/settings.py
@@ -145,14 +145,8 @@ STATICFILES_DIRS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-        
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     ),
 }
 
